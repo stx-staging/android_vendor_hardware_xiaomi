f856c4e2c509ca341f4f5c35c9d07bb16b9b0d54
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-13 16:17:38 +0600

    hardware/xiaomi: initial change for vendor relocation
    Change-Id: I006f5e445c5faf5bf538306eb37c53068ed1b209
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,7 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.goodix:hardware/xiaomi/interfaces/goodix"
+  "vendor.goodix:vendor/hardware/xiaomi/interfaces/goodix"
 
 do_makefiles_update \
-  "vendor.xiaomi:hardware/xiaomi/interfaces/xiaomi"
+  "vendor.xiaomi:vendor/hardware/xiaomi/interfaces/xiaomi"
