StatiXOS/android_hardware_xiaomi

powershare: rewrite in rust using AIDL API
Change-Id: Ibc9474cea7bad43d41c7f0afe6fc3b649e1e8712
Co-authored-by: Vaisakh Murali <mvaisakh@statixos.com>
Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

aidl/powershare/product.mk
@@ -1,0 +1,7 @@
+# Powershare
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/hardware/xiaomi/aidl/powershare/framework_compatibility_matrix.xml
+
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare-service.xiaomi
+
+BOARD_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/powershare

stx-staging/android_vendor_hardware_xiaomi

aidl/powershare/product.mk
@@ -1,0 +1,7 @@
+# Powershare
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/hardware/xiaomi/aidl/powershare/framework_compatibility_matrix.xml
+
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare-service.xiaomi
+
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/powershare