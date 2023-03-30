6586bacb512fba845a05ab899f5f50e2be3df747
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-30 16:33:16 +0600

    hidl: sensors: Create a split makefile for sensor product
    Change-Id: I637a0e2a516c896e3a8ea93f256f4d35d49ce2e1
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ hidl/sensors/sensors_product.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023 StatiXOS
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.1-service.xiaomi-multihal
+
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/sensors
