17b276e7f002165b578d9c3d1a60e3555873b8ef
Vaisakh Murali <mvaisakh@statixos.com>
2023-04-20 14:41:54 +0000

    hidl/sensors: split product for 2.1 and 1.0
    This will build 2.1 sensor hal by default unless
    TARGET_USES_XIAOMI_SENSOR_HAL_1_0 is set to true.
    
    Change-Id: I23b22eadbdef69bea3cf2f8e5d56b24942ab2711
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- hidl/sensors/sensors_product.mk
+++ hidl/sensors/sensors_product.mk
@@ -3,7 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifneq ($(TARGET_USES_XIAOMI_SENSOR_HAL_1_0),true)
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal
+else
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl-xiaomi
+endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/sensors
