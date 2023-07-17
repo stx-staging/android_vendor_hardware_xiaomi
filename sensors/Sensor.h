yaap/hardware_xiaomi

sensors: Implement single tap sensor
- This is a new and modernized sensor according to AOSP standards.

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

sensors/Sensor.h
@@ -127,6 +127,17 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
               static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 1)) {}
 };
 
+const std::string kTsSingleTapPressedPath = "/sys/devices/platform/soc/884000.i2c/i2c-1/1-0038/single_tap_pressed";
+
+class SingleTapSensor : public SysfsPollingOneShotSensor {
+  public:
+    SingleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+              sensorHandle, callback, kTsSingleTapPressedPath,
+              "Single Tap Sensor", "org.yaap.sensor.single_tap",
+              static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 1)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1

stx-staging/android_vendor_hardware_xiaomi

Change-Id: I7d3e5858016816e0af86de8cc619f3a0385f5481

sensors/Sensor.h
@@ -127,6 +127,17 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
               static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 1)) {}
 };
 
+const std::string kTsSingleTapPressedPath = "/sys/devices/platform/soc/884000.i2c/i2c-1/1-0038/single_tap_pressed";
+
+class SingleTapSensor : public SysfsPollingOneShotSensor {
+  public:
+    SingleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
+        : SysfsPollingOneShotSensor(
+              sensorHandle, callback, kTsSingleTapPressedPath,
+              "Single Tap Sensor", "com.statixos.sensor.single_tap",
+              static_cast<SensorType>(static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 1)) {}
+};
+
 }  // namespace implementation
 }  // namespace subhal
 }  // namespace V2_1