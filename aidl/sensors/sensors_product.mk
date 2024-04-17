c7cafbe6497c71d8c0451ed7bcaf8f3585c6308f
ralph950412 <ralph950412@gmail.com>
2024-04-18 02:41:50 +0800

    aidl: sensors: Create a makefile for AIDL sensor
    * Add sepolicy as well
    
    Change-Id: I90a59c8a5af8806f2d0930529c8d5e64850a4dde

---
+++ aidl/sensors/sensors_product.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 StatiXOS
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.xiaomi-multihal
+
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/hardware/xiaomi/sepolicy/sensors
