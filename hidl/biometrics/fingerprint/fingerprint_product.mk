a29549927e2ff0c298447bee17edede91645a6df
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-04-25 18:17:56 +0530

    hidl: fingerprint: Initial split makefile
    Change-Id: I32a59c7dc9a28a100bdcaeb1387483f80fc00d69

---
+++ hidl/biometrics/fingerprint/fingerprint_product.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 StatiXOS
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Fingerprint split product makefile
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
